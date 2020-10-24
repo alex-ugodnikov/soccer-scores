a76e69d0eca248b2a00acf0d49c3026acca0b924
alexugo <alex.ugodnikov@gmail.com>
2020-10-24 14:56:59 -0400

    Match and List components done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import List from './components/List';
 import Navbar from './components/Navbar';
 import Match from './components/Match';
-import { BrowserRouter as Router, Route, Switch, Link, BrowserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 import './App.css';
@@ -11,14 +11,19 @@ import './App.css';
 
 class App extends Component {
 	state = {
-		matchesFromAPI: []
+		matchesFromAPI: [],
+		loading: true
 	};
 
 	componentDidMount() {
-		axios.get('https://www.scorebat.com/video-api/v1/').then(result => {
-			const matchesFromAPI = result.data;
-			this.setState({ matchesFromAPI });
-		});
+		axios
+			.get('https://www.scorebat.com/video-api/v1/')
+			.then(result => {
+				const matchesFromAPI = result.data;
+				this.setState({ matchesFromAPI });
+			})
+			.then(() => this.setState({ loading: false }))
+			.catch(err => console.log(err));
 	}
 
 	render() {
@@ -26,11 +31,22 @@ class App extends Component {
 			<div className="App">
 				<Navbar />
 				<Router>
-					<Switch>
-						{/* <Route path="/" exact component={List} matchesFromAPI={this.state.matchesFromAPI} /> */}
-						<Route path="/" exact component={() => <List matchesFromAPI={this.state.matchesFromAPI} />} />
-						<Route path="/match/:id" component={() => <Match matchesFromAPI={this.state.matchesFromAPI} />} />
-					</Switch>
+					{this.state.loading ? (
+						'Loading...'
+					) : (
+						<Switch>
+							{/* <Route path="/" exact component={List} matchesFromAPI={this.state.matchesFromAPI} /> */}
+							<Route
+								path="/"
+								exact
+								component={() => <List matchesFromAPI={this.state.matchesFromAPI} />}
+							/>
+							<Route
+								path="/match/:id"
+								component={props => <Match {...props} matchesFromAPI={this.state.matchesFromAPI} />}
+							/>
+						</Switch>
+					)}
 				</Router>
 			</div>
 		);
--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, BrowserHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Match extends Component {
 	render() {
 
-        //const match = this.props.matchesFromApi[this.props.params.id];
-        const id = this.props.params.id;
-        console.log(id)
+        const {id} = this.props.match.params;
+        const match = this.props.matchesFromAPI[id];
+        console.log(match);
 
 		return (
 			<div>
@@ -14,6 +14,21 @@ class Match extends Component {
 				<Link to={'/'}>
 					<button>Go to home</button>
 				</Link>
+
+                <div className="match-card">
+								<p>{match.title}</p>
+								<p className="small">{match.competition.name}</p>
+
+								{match.videos.map(matchVideo => {                                       
+										return (
+											<div className="video-container">
+												<p>{matchVideo.title}</p>
+												<div dangerouslySetInnerHTML={{__html: matchVideo.embed}}></div>
+											</div>
+										);
+									})}
+                                    
+							</div>                
 			</div>
 		);
 	}
